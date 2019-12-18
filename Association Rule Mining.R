1d165c57068e5ee6e5a11004fc9162f9b50f5ff4
Jarrad Denzel Washington <jarraddenzel@gmail.com>
2019-12-18 14:22:08 -0600

    Add files via upload

---
+++ Association Rule Mining.R
@@ -0,0 +1,6 @@
+# Association Rules // Market Basket Analysis
+# aims to find the rules which enable us to predict the occurrence 
+# of a specific item based on the occurrences of the other items 
+# in the transaction.
+
+# Applications: Basket data analysis, Cross marketing, Catalog design
